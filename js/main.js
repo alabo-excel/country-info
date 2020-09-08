a848592569d6b68e0aea7b33cc9aaa8dddb14385
alabo-excel <iamalaboexcel@gmail.com>
2020-09-08 15:15:07 +0100

    added filters functionality

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ var light = document.getElementById('light')
 
 var dark = document.getElementById('dark')
 
+var asia = document.getElementById('asia')
+var africa = document.getElementById('africa')
+var europe = document.getElementById('europe')
+var oceania = document.getElementById('oceania')
+
 
 
 
@@ -92,4 +97,124 @@ light.addEventListener('click', () => {
     dark.style.display = 'block'
     light.style.display = 'none'
 
+})
+
+
+
+asia.addEventListener('click', () => {
+    spinner.style.display = 'block'
+    document.getElementById('container').innerHTML = ""
+    fetch('https://restcountries.eu/rest/v2/region/asia')
+
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log(data)
+        data.forEach(function(data) {
+            document.getElementById('container').innerHTML += `
+        <div id="content" class="col-lg-3 m-4 text-capitalize">
+        <img src="${data.flag}">
+        <strong>
+        <p>${data.name}</p>
+        </strong>
+        <p>population: ${data.population}</p>
+        <p>region: ${data.region}</p>
+        <p>capital: ${data.capital}</p>
+        </div>
+        `
+            spinner.style.display = 'none'
+
+        })
+    })
+})
+
+
+africa.addEventListener('click', () => {
+    spinner.style.display = 'block'
+    document.getElementById('container').innerHTML = ""
+    fetch('https://restcountries.eu/rest/v2/region/africa')
+
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log(data)
+        data.forEach(function(data) {
+            document.getElementById('container').innerHTML += `
+        <div id="content" class="col-lg-3 m-4 text-capitalize">
+        <img src="${data.flag}">
+        <strong>
+        <p>${data.name}</p>
+        </strong>
+        <p>population: ${data.population}</p>
+        <p>region: ${data.region}</p>
+        <p>capital: ${data.capital}</p>
+        </div>
+        `
+            spinner.style.display = 'none'
+
+        })
+    })
+})
+
+
+europe.addEventListener('click', () => {
+    spinner.style.display = 'block'
+    document.getElementById('container').innerHTML = ""
+    fetch('https://restcountries.eu/rest/v2/region/europe')
+
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log(data)
+        data.forEach(function(data) {
+            document.getElementById('container').innerHTML += `
+        <div id="content" class="col-lg-3 m-4 text-capitalize">
+        <img src="${data.flag}">
+        <strong>
+        <p>${data.name}</p>
+        </strong>
+        <p>population: ${data.population}</p>
+        <p>region: ${data.region}</p>
+        <p>capital: ${data.capital}</p>
+        </div>
+        `
+            spinner.style.display = 'none'
+
+        })
+    })
+})
+
+oceania.addEventListener('click', () => {
+    spinner.style.display = 'block'
+    document.getElementById('container').innerHTML = ""
+    fetch('https://restcountries.eu/rest/v2/region/oceania')
+
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log(data)
+        data.forEach(function(data) {
+            document.getElementById('container').innerHTML += `
+        <div id="content" class="col-lg-3 m-4 text-capitalize">
+        <img src="${data.flag}">
+        <strong>
+        <p>${data.name}</p>
+        </strong>
+        <p>population: ${data.population}</p>
+        <p>region: ${data.region}</p>
+        <p>capital: ${data.capital}</p>
+        </div>
+        `
+            spinner.style.display = 'none'
+
+        })
+    })
 })
